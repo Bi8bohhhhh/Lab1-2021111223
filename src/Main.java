8740287e55f7390b4c022b31b718994ca7835921
wgz <2776170210@qq.com>
2024-05-24 17:30:00 +0800

    branch change1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
-//git modified try
+//git modified try66666
+//branch change1
 public class Main {
     public static void main(String[] args) {
         //读入文件并转化为标准形式，存入graph中
