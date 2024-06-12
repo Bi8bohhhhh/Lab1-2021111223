02af47f8b320193c92b5c4e02800eb65dc67cd62
wgz <2776170210@qq.com>
2024-06-12 20:12:11 +0800

    lab3b

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,40 +1,40 @@
 public class LinkedList {
-  private Node head = null;
-  private Node tail = null;
-  public int nodeNum;
+    private Node head = null;
+    private Node tail = null;
+    public int nodeNum;
 
-  public LinkedList() {
-    nodeNum = -1;
-  }
-
-  public LinkedList(String w) {
-    nodeNum = 0;
-    Node newNode = new Node(w, -1);
-    head = newNode;
-    tail = newNode;
-  }
-
-  public boolean isEmpty() {
-
-    return head == null;
-  }
-
-  public void addNode(String w, int n) {
-    Node newNode = new Node(w, n);
-    if (isEmpty()) {
-      head = tail = newNode;
-    } else {
-      tail.next = newNode;
-      tail = newNode;
+    public LinkedList() {
+        nodeNum = -1;
     }
-    nodeNum++;
-  }
 
-  public Node getHead() {
-    return head;
-  }
+    public LinkedList(String w) {
+        nodeNum = 0;
+        Node newNode = new Node(w, -1);
+        head = newNode;
+        tail = newNode;
+    }
 
-  public Node getTail() {
-    return tail;
-  }
+    public boolean isEmpty() {
+
+        return head == null;
+    }
+
+    public void addNode(String w, int n) {
+        Node newNode = new Node(w, n);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        nodeNum++;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,19 +1,19 @@
 public class Node {
-  public String word; //内容
-  public int weight;  //权重
-  public Node next;   //下一个节点
-  public int num;     //邻接表位置
+    public String word; //内容
+    public int weight;  //权重
+    public Node next;   //下一个节点
+    public int num;     //邻接表位置
 
-  public Node() {
-    word = null;
-    next = null;
-    num = weight = 0;
-  }
+    public Node() {
+        word = null;
+        next = null;
+        num = weight = 0;
+    }
 
-  public Node(String w, int n) {
-    word = w;
-    weight = 1;
-    next = null;
-    num = n;
-  }
+    public Node(String w, int n) {
+        word = w;
+        weight = 1;
+        next = null;
+        num = n;
+    }
 }
